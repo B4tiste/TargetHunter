cf9d892ec4a8b2518c1512883b8bd8462cdcd420
B4tiste <64788456+B4tiste@users.noreply.github.com>
2021-01-28 21:36:32 +0100

    Add files via upload

---
+++ TargetHunter.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+TargetHunter game code
+
+@author: B4tiste
+"""
+
+from ursina import * #Import all the Urdina lib
+import random
+import time
+import os
+
+clear = lambda : os.system('cls')
+
+SPEED = 5
+startup_position = (0, 0)
+size_player = (1, 1)
+size_target = (0.5, 0.5)
+GAP = 0.75
+DELAY = 0
+score = 0
+hits = 0
+time.sleep(DELAY)
+rem_time = 30
+cpt = 0
+
+def update():
+
+	global cpt
+	global score
+	global hits
+	global SPEED
+	global rem_time
+	
+	cpt = cpt + 1
+	
+	if cpt == 60 :
+		rem_time = rem_time - 1
+		cpt = 0
+	
+	info.text = 'Score = ' + str(score) + '\nTime remaining : ' + str(rem_time) + 's'
+
+	if held_keys['z']:#Go Up
+		player.y = player.y + SPEED * time.dt
+	if held_keys['q']:#Go to the Left
+		player.x = player.x - SPEED * time.dt
+	if held_keys['s']:#Go Down
+		player.y = player.y - SPEED * time.dt
+	if held_keys['d']:#Go to the Right
+		player.x = player.x + SPEED * time.dt
+	"""if held_keys['r']:#Reset the position of the player
+		player.x = 0
+		player.y = 0"""
+	if held_keys['escape']:
+		exit()
+	
+	print('\nx = ' + str(player.x))
+	print('y = ' + str(player.y))
+	
+	if player.x < (target.x + GAP) and player.x > (target.x - GAP):
+		if player.y < (target.y + GAP) and player.y > (target.y - GAP):
+			x_rdm = random.randint(-5, 5)
+			y_rdm = random.randint(-3, 3)
+			
+			target.x = x_rdm
+			target.y = y_rdm
+			
+			score = score + 1
+			hits = hits + 1
+		
+			info.text = 'Score = ' + str(score) + '\nTime remaining : ' + str(rem_time) + 's'
+			
+			SPEED = SPEED + 1
+	
+	if player.x < -7 or player.x > 7:
+		
+		score = score - 1
+		info.text = 'Score = ' + str(score) + '\nTime remaining : ' + str(rem_time) + 's'
+		
+		player.position = startup_position
+		
+		SPEED = SPEED - 1
+
+	if player.y < -4 or player.y > 4:
+		
+		score = score - 1
+		info.text = 'Score = ' + str(score) + '\nTime remaining : ' + str(rem_time) + 's'
+		
+		player.position = startup_position
+		
+		SPEED = SPEED - 1
+		
+	if rem_time == 0:
+	
+		clear()
+		
+		print("\nFinal Score = " + str(score))
+		print("\nTarget hit = " + str(hits))
+		print("\nYou touched " + str(hits-score) + ' times the side !')
+		
+		exit()
+	
+
+#Create an instance of Ursina ~Window
+app = Ursina()
+
+# Usina use Entities, 'elements' that can be almost anything in the instance
+# [entity_name] = Entity(model = 'model', color = color.COLOR_NAME, scale = (x, y), position = (x, y))
+# The center of the window is the point with (x, y) = (0, 0)
+
+player = Entity(model = 'quad', color = color.green, scale = size_player, position = startup_position)
+
+target = Entity(model = 'quad', color = color.red, scale = size_target, position = (1, 1))
+
+info = Text(text = 'Score', origin = (0, 0), color = color.white)
+
+#Start the instance created at l.109
+app.run()
